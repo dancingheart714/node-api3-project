f270954a0c2f6ba27d5b1999d30e2186658bcdf5
Leah King <dancingheart714@yahoo.com>
2021-05-05 14:58:09 -0400

    server and index set up

--- api/server.js
+++ api/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
+const hubsRouter = require('./hubs/hubs-router.js');
+
 const server = express();
+
+server.use(express.json());
+
+server.use('./api/hubs', hubsRouter);
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`
+  <h2>Let's write some middleware!</h2>
+  `);
 });
 
 module.exports = server;
