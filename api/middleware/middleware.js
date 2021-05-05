2bf4c9876414307609fb49d8d79a11ec0fdaad56
Leah King <dancingheart714@yahoo.com>
2021-05-05 15:58:59 -0400

    logger & validateUser done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,64 @@
+const users = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  return (req, res, next) => {
+    console.log(`
+  Method: ${req.method},
+  URL: ${req.url},
+  Timestamp: ${Date().toISOSString()}
+  `);
+    next();
+  };
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  return (req, res, next) => {
+    users
+      .getById(req.params.id)
+      .then((user) => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(404).json({
+            message: 'user not found',
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(error);
+      });
+  };
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  return (req, res, next) => {
+    if (!req.body.name) {
+      res.status(400).json({
+        message: 'missing required name field',
+      });
+    }
+  };
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  return (req, res, next) => {
+    if (!req.body.text) {
+      res.status(400).json({
+        message: 'missing required text field',
+      });
+    } else {
+      res.postEdit = res.body;
+      next();
+    }
+  };
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validatePost,
+  validateUser,
+  validateUserId,
+};
--- api/server.js
+++ api/server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const userRouter = require('./users/users-router');
+
+const { logger } = require('./middleware/middleware');
 
 const server = express();
 
+server.use(logger);
+server.use(userRouter);
 server.use(express.json());
 
-server.use('./api/hubs', hubsRouter);
-
-// remember express by default cannot parse JSON in request bodies
-
-// global middlewares and the user's router need to be connected here
-
 server.get('/', (req, res) => {
-  res.send(`
-  <h2>Let's write some middleware!</h2>
-  `);
+  res.status(200).json({
+    message: 'Successful',
+  });
 });
 
 module.exports = server;
